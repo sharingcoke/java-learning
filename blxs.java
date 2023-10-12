34ee42a67c2b883c6c441153a73f1e394e7be399
sharingcoke <junhuali322@gmail.com>
2023-10-12 15:24:30 +0800

    保留一位小数

--- blxs.java
+++ blxs.java
@@ -1,9 +1,16 @@
 import java.util.Scanner;
-public class hello {
+public class blxs {
     
     public static void main(String[] args) {
+        double x;
         Scanner sc = new Scanner(System.in);//构造一个Scanner对象，其传入参数为System.in
         System.out.println("请输入数值");
-        double x=sc.nextDouble();
+        x=sc.nextDouble()+0.05;
+        int d1=(int)(x*10);
+        double d2=d1/10.0;
+        //其他方法
+        //double d3=x+0.05;
+        System.out.println(d2);
+
     }
 }
