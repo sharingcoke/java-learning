93fc0c12d7a234bee402c60bef7a300b19e4092f
sharingcoke <junhuali322@gmail.com>
2023-10-12 15:05:23 +0800

    注释

--- hello.java
+++ hello.java
@@ -1,3 +1,9 @@
+import java.util.Scanner;
 public class hello {
-
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);//构造一个Scanner对象，其传入参数为System.in
+        System.out.println("请输入数值");
+        double x=sc.nextDouble();
+    }
 }
