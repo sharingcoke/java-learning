ff692c628feec28c99af41ec97f9727713289769
sharingcoke <junhuali322@gmail.com>
2023-10-13 09:17:45 +0800

    注释

---
+++ tese.java
@@ -0,0 +1,10 @@
+public class tese {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        byte b1 = 10;//短整型 8位 -128至127
+
+        char c = ' ';//有且仅有一个字符
+        int n2 = c;//字符串可以是整型，反之，可能不成立
+
+    }
+}
