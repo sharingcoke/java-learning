11b228b29c840a2392a6a1909024f70b5e2453ba
sharingcoke <junhuali322@gmail.com>
2023-10-26 15:02:01 +0800

    注释

--- 2023.10.26/test6.java
+++ 2023.10.26/test6.java
@@ -21,6 +21,21 @@ public class test6 {
             case 11:x=(m-1)*30+4+d;break;
             case 12:x=(m-1)*30+4+d;break;
         }
+        /*
+        switch(m){
+            case 12:x+=30;
+            case 11:x+=31;
+            case 10:x+=30;
+            case 9:x+=31;
+            case 8:x+=31;
+            case 7:x+=30;
+            case 6:x+=31;
+            case 5:x+=30;
+            case 4:x+=31;
+            case 3:x+=28;
+            case 2:x+=31;
+            case 1:x+=d;
+        }没有break语句就会一直走下去，这个运用了switch的特点*/
         if((y%4==0&y%100!=0)|((y%400==0)))
             if(m>2) x=x+1;
         System.out.println("今年已经过去"+x+"天");
